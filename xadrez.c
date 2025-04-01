79a87550d8c8f394905149120bcce9eb49938202
Theuzindeveloper <131308267+Matheusleitelima@users.noreply.github.com>
2025-04-01 00:19:22 +0000

    Adicionando os Loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
+
     printf("Movimento da torre \n");
-    for (int i = 1; i<= 5; i++){
+    for (int i = 1; i <= 5; i++) {
         printf("Direita \n");
     }
 
-    printf("Movimento bispo \n");
+
+    printf("Movimento do bispo \n");
     int m = 1;
-     while(m <= 5){
+    while (m <= 5) {
         printf("Cima e direita \n");
         m++;
-     }
+    }
 
 
-    printf("Movimneto rainha \n");
-     int r = 1;
-     do{
+    printf("Movimento da rainha \n");
+    int r = 1;
+    do {
         printf("Esquerda \n");
         r++;
-     }while(r <= 8);
+    } while (r <= 8);
 
 
-     return 0;
+    printf("\n");
+
+
+    printf("Movimento do cavalo \n");
+
+
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo \n");
+        for (int j = 0; j < 1; j++) {
+            if (i == 1) {
+                printf("Esquerda \n");
+            }
+        }
+    }
+
+    return 0;
 }
